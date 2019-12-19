8c2373f5b35ca3549e12c2458ea8474851557732
Tiffany <tiffquinlan@gmail.com>
2019-12-18 18:41:22 -0700

    Update Food.java

--- src/TeamProject/Food.java
+++ src/TeamProject/Food.java
@@ -42,10 +42,7 @@ public class Food {
 
     /** Return a string representation of this object */
     public String toString() {
-//        String.format("Item %s, %t Cost: $%f.2", name, cost);
-//        return  "\nItem: " + name + "\t\tCost: $" + getCost();
         return String.format("%nItem %s \tCost: $%.2f", name, getCost());
-        //        System.out.printf( "%-15s%-10s%-40s%-5s\n", "", "", "salami, tomatoes, olives, anchovies", "+$1.00 for each extra topping" );
     }
 
 }
